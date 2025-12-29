cce5372082ec82f1937b200f4ccfaa48ef6fd7f0
Devanshu <devanshukumar103@gmail.com>
2025-12-29 17:14:20 +0530

    Initial React app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>🚀 React App Deployed via GitHub Actions</h1>
+      <p>CI/CD using GitHub Pages</p>
     </div>
   );
 }
